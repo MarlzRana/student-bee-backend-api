97ca9eb78104eeee1c02b08cc3b39a166e57fea3
Marlin Ranasinghe <dinethmarlinranasinghe@gmail.com>
2022-02-22 19:48:43 +0000

    Setup SQL database connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Imported packages
 const express = require('express');
 const mysql = require('mysql');
+const fs = require('fs');
 require('dotenv').config();
 
 //Environmental variables
@@ -9,6 +10,16 @@ const app = express();
 
 //Package parameter setup
 app.use(express.json());
+
+//Setting up the database connection
+var db = mysql.createConnection({
+  host: process.env.DBHOSTSERVERADDRESS,
+  user: process.env.DBSERVERUSERNAME,
+  password: process.env.DBSERVERPASSWORD,
+  database: process.DBSERVERDATABASENAME,
+  port: process.env.DBSERVERPORT,
+  ssl: { ca: fs.readFileSync(process.env.DBCERTIFICATEFILEPATH) },
+});
 
 app.listen(PORT, () =>
   console.log(`The server is running and listening on port: ${PORT}`)
