456c3f171692b45ac5d087d146cd105775955646
Marlin Ranasinghe <dinethmarlinranasinghe@gmail.com>
2022-02-23 14:14:33 +0000

    Implemented registration functionality

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs');
+const cors = require('cors');
 require('dotenv').config();
 
-//Environmental variables
+//Package setup
+
+// //Environmental variables
 const PORT = process.env.PORT;
 const app = express();
 
-//Package parameter setup
+// //Package parameter setup
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //Setting up the database connection
 var db = mysql.createConnection({
@@ -27,6 +32,22 @@ db.connect((err) => {
   }
   console.log(
     `The server has successfully connected to ${process.env.DBSERVERDATABASENAME}`
+  );
+});
+
+app.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  db.query(
+    'INSERT INTO studentbee.tbl_user_login_information (username, password) VALUES (?, ?)',
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send({ status: 'SUCCESS' });
+      }
+    }
   );
 });
 
