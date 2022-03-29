35fe3204e4bd0df3a483ca734c4301eb1930b589
Marlin Ranasinghe <dinethmarlinranasinghe@gmail.com>
2022-03-29 20:55:13 +0100

    Ability to search for an event

--- routes/eventsSystem.js
+++ routes/eventsSystem.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 //Imported packages
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 
 //Importing local file dependencies
-const validation = require("../validation/validation");
-const tbl_events = require("../database/tbl_events");
-const tbl_user_login_information = require("../database/tbl_user_login_information");
+const validation = require('../validation/validation');
+const tbl_events = require('../database/tbl_events');
+const tbl_user_login_information = require('../database/tbl_user_login_information');
 //Environmental variables
 
 //Package setup
@@ -15,19 +15,19 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 //A default gateway to test if API server is accessible
-router.route("/").get((req, res) => {
+router.route('/').get((req, res) => {
   return res.send({
-    message: "Default gateway of student-bee-backend-api route:/eventsSystem",
+    message: 'Default gateway of student-bee-backend-api route:/eventsSystem',
   });
 });
 
-router.route("/addEvent").post(async (req, res) => {
+router.route('/addEvent').post(async (req, res) => {
   try {
     //Check if the user is logged in
     if (!req.session.user) {
       return res.send({
-        status: "failure",
-        message: "notLoggedIn",
+        status: 'failure',
+        message: 'notLoggedIn',
       });
     }
     //Get the parameters provided in the response
@@ -52,8 +52,8 @@ router.route("/addEvent").post(async (req, res) => {
       descriptionIn === null
     ) {
       return res.send({
-        status: "failure",
-        message: "missingParameters",
+        status: 'failure',
+        message: 'missingParameters',
         parameterPresenceCheckDetails: {
           title: titleIn !== null,
           startDateTime: startDateTimeIn !== null,
@@ -91,8 +91,8 @@ router.route("/addEvent").post(async (req, res) => {
       )
     ) {
       return res.send({
-        status: "failure",
-        reason: "invalidInputFormat",
+        status: 'failure',
+        reason: 'invalidInputFormat',
         validationCheckDetails: validationCheckDetails,
       });
     }
@@ -109,25 +109,25 @@ router.route("/addEvent").post(async (req, res) => {
       descriptionIn
     );
     return res.send({
-      status: "success",
-      message: "eventAdded",
+      status: 'success',
+      message: 'eventAdded',
     });
   } catch (err) {
     console.log(err);
     return res.send({
-      status: "failure",
-      reason: "internalError",
+      status: 'failure',
+      reason: 'internalError',
     });
   }
 });
 
-router.route("/top10MostRecentEvents").get(async (req, res) => {
+router.route('/top10MostRecentEvents').get(async (req, res) => {
   try {
     //Check if the user is logged in
     if (!req.session.user) {
       return res.send({
-        status: "failure",
-        reason: "notLoggedIn",
+        status: 'failure',
+        reason: 'notLoggedIn',
       });
     }
 
@@ -137,18 +137,18 @@ router.route("/top10MostRecentEvents").get(async (req, res) => {
     const arrOfObjToSend = await Promise.all(
       dbResult.map(async (row) => {
         const usernameForParticularUserID =
-          await tbl_user_login_information.getUsernameByUserID(row["user_id"]);
+          await tbl_user_login_information.getUsernameByUserID(row['user_id']);
         return {
-          eventID: row["event_id"],
+          eventID: row['event_id'],
           username: usernameForParticularUserID,
-          title: row["title"],
-          startDateTime: row["start_datetime"],
-          endDateTime: row["end_datetime"],
-          location: row["location"],
-          organizerName: row["organizer_name"],
-          contactEmail: row["contact_email"],
-          contactPhoneNumber: row["contact_phone_number"],
-          description: row["description"],
+          title: row['title'],
+          startDateTime: row['start_datetime'],
+          endDateTime: row['end_datetime'],
+          location: row['location'],
+          organizerName: row['organizer_name'],
+          contactEmail: row['contact_email'],
+          contactPhoneNumber: row['contact_phone_number'],
+          description: row['description'],
         };
       })
     );
@@ -156,19 +156,19 @@ router.route("/top10MostRecentEvents").get(async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.send({
-      status: "failure",
-      reason: "internalError",
+      status: 'failure',
+      reason: 'internalError',
     });
   }
 });
 
-router.route("/getEventDetails").post(async (req, res) => {
+router.route('/getEventDetails').post(async (req, res) => {
   try {
     //Check if the user is logged in
     if (!req.session.user) {
       return res.send({
-        status: "failure",
-        reason: "notLoggedIn",
+        status: 'failure',
+        reason: 'notLoggedIn',
       });
     }
 
@@ -179,16 +179,16 @@ router.route("/getEventDetails").post(async (req, res) => {
 
     if (!validID) {
       return res.send({
-        status: "failure",
-        reason: "Invalid ID format",
+        status: 'failure',
+        reason: 'Invalid ID format',
       });
     }
 
     const dbResult = await tbl_events.getEventInformation(enteredEventID);
     if (dbResult === undefined) {
       return res.send({
-        status: "failure",
-        reason: "This event does not exist",
+        status: 'failure',
+        reason: 'This event does not exist',
       });
     }
 
@@ -206,23 +206,23 @@ router.route("/getEventDetails").post(async (req, res) => {
     };
 
     return res.send({
-      status: "success",
+      status: 'success',
       eventInformation: returnedInformation,
     });
   } catch (error) {
     return res.send({
-      status: "error",
+      status: 'error',
     });
   }
 });
 
-router.route("/editEventDetails").post(async (req, res) => {
+router.route('/editEventDetails').post(async (req, res) => {
   try {
     //Check if the user is logged in
     if (!req.session.user) {
       return res.send({
-        status: "failure",
-        reason: "notLoggedIn",
+        status: 'failure',
+        reason: 'notLoggedIn',
       });
     }
 
@@ -248,8 +248,8 @@ router.route("/editEventDetails").post(async (req, res) => {
       descriptionIn === null
     ) {
       return res.send({
-        status: "failure",
-        message: "missingParameters",
+        status: 'failure',
+        message: 'missingParameters',
         parameterPresenceCheckDetails: {
           title: titleIn !== null,
           startDateTime: startDateTimeIn !== null,
@@ -287,8 +287,8 @@ router.route("/editEventDetails").post(async (req, res) => {
       )
     ) {
       return res.send({
-        status: "failure",
-        reason: "invalidInputFormat",
+        status: 'failure',
+        reason: 'invalidInputFormat',
         validationCheckDetails: validationCheckDetails,
       });
     }
@@ -298,16 +298,16 @@ router.route("/editEventDetails").post(async (req, res) => {
 
     if (!validID) {
       return res.send({
-        status: "failure",
-        reason: "Invalid ID format",
+        status: 'failure',
+        reason: 'Invalid ID format',
       });
     }
 
     const dbResult = await tbl_events.getEventInformation(enteredEventID);
     if (dbResult === undefined) {
       return res.send({
-        status: "failure",
-        reason: "This event does not exist",
+        status: 'failure',
+        reason: 'This event does not exist',
       });
     }
 
@@ -315,8 +315,8 @@ router.route("/editEventDetails").post(async (req, res) => {
     const userID = req.session.user.userID;
     if (dbResult.user_id !== userID) {
       return res.send({
-        status: "failure",
-        reason: "You do not own this event",
+        status: 'failure',
+        reason: 'You do not own this event',
       });
     }
 
@@ -334,23 +334,23 @@ router.route("/editEventDetails").post(async (req, res) => {
     );
 
     return res.send({
-      status: "success",
-      message: "Event successfully edited",
+      status: 'success',
+      message: 'Event successfully edited',
     });
   } catch (error) {
     return res.send({
-      status: "error",
+      status: 'error',
     });
   }
 });
 
-router.route("/ownsEvent").post(async (req, res) => {
+router.route('/ownsEvent').post(async (req, res) => {
   try {
     //Check if the user is logged in
     if (!req.session.user) {
       return res.send({
-        status: "failure",
-        reason: "notLoggedIn",
+        status: 'failure',
+        reason: 'notLoggedIn',
       });
     }
 
@@ -360,8 +360,8 @@ router.route("/ownsEvent").post(async (req, res) => {
     const dbResult = await tbl_events.getEventInformation(eventID);
     if (dbResult === undefined) {
       return res.send({
-        status: "failure",
-        reason: "This event does not exist",
+        status: 'failure',
+        reason: 'This event does not exist',
       });
     }
 
@@ -370,26 +370,94 @@ router.route("/ownsEvent").post(async (req, res) => {
 
     if (!validID) {
       return res.send({
-        status: "failure",
-        reason: "Invalid ID format",
+        status: 'failure',
+        reason: 'Invalid ID format',
       });
     }
 
     if (dbResult.user_id !== userID) {
       return res.send({
-        status: "success",
+        status: 'success',
         owned: false,
       });
     } else {
       return res.send({
-        status: "success",
+        status: 'success',
         owned: true,
       });
     }
   } catch (error) {
     console.log(error);
     return res.send({
-      status: "error",
+      status: 'error',
+    });
+  }
+});
+
+router.route('/search').post(async (req, res) => {
+  try {
+    //Check if the user is logged in
+    if (!req.session.user) {
+      return res.send({
+        status: 'failure',
+        reason: 'notLoggedIn',
+      });
+    }
+    //Get the parameter
+    const query = req.body.query;
+    //Performing a presence check
+    const parameterPresenceCheckDetails = {
+      query: query !== null && query !== undefined,
+    };
+    if (
+      !Object.keys(parameterPresenceCheckDetails).every(
+        (key) => parameterPresenceCheckDetails[key]
+      )
+    ) {
+      return res.send({
+        status: 'failure',
+        message: 'missingParameters',
+        parameterPresenceCheckDetails: parameterPresenceCheckDetails,
+      });
+    }
+    //Validating the query string
+    const validationCheckDetails = {
+      query: validation.validateLongName(query),
+    };
+    if (
+      !Object.keys(validationCheckDetails).every((key) => {
+        return validationCheckDetails[key];
+      })
+    ) {
+      return res.send({
+        status: 'failure',
+        reason: 'invalidInputFormat',
+        validationCheckDetails: validationCheckDetails,
+      });
+    }
+    //Searching for an event with a name that begins with query
+    const dbResult = await tbl_events.findRecordsByTitle(query);
+    return res.send(
+      dbResult.map((obj) => {
+        return {
+          eventID: obj.event_id,
+          userID: obj.user_id,
+          title: obj.title,
+          startDatetime: obj.start_datetime,
+          endDatetime: obj.end_datetime,
+          location: obj.location,
+          organizerName: obj.organizer_name,
+          contactEmail: obj.contact_email,
+          contactPhoneNumber: obj.contact_phone_number,
+          description: obj.description,
+        };
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    return res.send({
+      status: 'failure',
+      reason: 'internalError',
     });
   }
 });
